d2332a0a1ecea007e30674116e6601a382fe0503
Ayush Gaur <77896630+theayushgaur@users.noreply.github.com>
2025-04-12 09:26:05 +0530

    Update - Change Typo Errors

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="mb-4 md:mb-0">
             <p className="text-sm text-muted-foreground">
-              © {new Date().getFullYear()} Made with Love By Ayush Gaur.
+              © {new Date().getFullYear()} Made By Ayush Gaur.
             </p>
           </div>
           <div className="flex space-x-4">
